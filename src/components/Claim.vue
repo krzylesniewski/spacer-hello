bd5e15b885779cf32b2af6c0d85711bbc4237940
krzylesniewski <krzysztof.lesniewski.wroc@gmail.com>
2018-07-18 00:56:10 +0200

    fixint all problem with pullrequest

---
+++ src/components/Claim.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="wrapper">
+        <h1 class="claim">spacer</h1>
+        <p class="subclaim">
+            Rozpocznij swoją podróż przez naszą wspaniałą galaktykę i odkryj miejśca, o któych nigdy nie słyszałeś
+        </p>
+        <p class="subclaim">
+            Wpisz cokolwiek związane z kosmosem
+        </p>
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="scss" scoped>
+    .wrapper{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
+
